47f070fef605b006c62b7cef3470e9141993768d
Calin <calinoiu.alexandru@agilefreaks.com>
2015-01-28 23:18:08 +0200

    updates config

--- puppet/modules/freaks/manifests/init.pp
+++ puppet/modules/freaks/manifests/init.pp
@@ -61,11 +61,10 @@ class freaks::mongo {
 class freaks::redis_server {
   class { 'freaks::newrelic_base': }
 
-  class { 'redis': version => $required_redis_version }->
-  redis::instance { 'redis':
-    redis_port => '6379',   
-    redis_bind_address => ['127.0.0.1',"$ipaddress_eth0"],
-    redis_max_memory   => '400mb',
+  class { 'redis':
+    port => '6379',   
+    bind => ['127.0.0.1',"$ipaddress_eth0"],
+    maxmemory   => '400000000',
   }
 }
 
