a111ea5d0d4709b8af0f475249170250082918c6
calin <calinoiu.alexandru@agilefreaks.com>
2015-06-17 10:32:19 +0300

    adds enviroment settings for staging

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -24,7 +24,13 @@ node /webstaging\d./ {
   class { 'freaks::web':
     gemset => 'webomni',
     app_name => 'webomni'
-  }  
+  }
+
+  $environment_variables = {
+    'GOOGLE_CLIENT_SECRET' =>  { ensure => present, value => '\'{"web":{"auth_uri":"https://accounts.google.com/o/oauth2/auth","client_secret":"4mLcVIVpj0zHuIR9sbmSPoSH","token_uri":"https://accounts.google.com/o/oauth2/token","client_email":"930634995806-vqol11op6ugtvljcftoqcc1v757gep8u@developer.gserviceaccount.com","redirect_uris":["https://webstaging.omnipasteapp.com/users/auth/google_oauth2/callback"],"client_x509_cert_url":"https://www.googleapis.com/robot/v1/metadata/x509/930634995806-vqol11op6ugtvljcftoqcc1v757gep8u@developer.gserviceaccount.com","client_id":"930634995806-vqol11op6ugtvljcftoqcc1v757gep8u.apps.googleusercontent.com","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","javascript_origins":["https://webstaging.omnipasteapp.com"]}}\'', }
+  }
+
+  create_resources(systemenv::var, $environment_variables)
 }
 
 node /webproduction\d./ {
